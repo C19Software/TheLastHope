cc09f84cc9f42d9b1ba279152d0c79713e1ecefb
C19Software <68974992+C19Software@users.noreply.github.com>
2020-08-03 19:20:21 -0400

    Update harley.cs

--- Juego PlataformaOriginal/Assets/harley.cs
+++ Juego PlataformaOriginal/Assets/harley.cs
@@ -15,8 +15,8 @@ public class harley : MonoBehaviour
         {
             Gizmos.color = Color.cyan;
             Gizmos.DrawLine(from.position, to.position);
-            Gizmos.DrawSphere(from.position, 0.15f);
-            Gizmos.DrawSphere(to.position, 0.15f);
+            Gizmos.DrawSphere(from.position, 0.3f);
+            Gizmos.DrawSphere(to.position, 0.3f);
         }
 
     }
